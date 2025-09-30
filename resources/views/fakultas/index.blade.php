6045586cb8ed3e36713ca37d56f7a3fce9b94078
Muhamad Nizar Rahman <dailyquote15@gmail.com>
2025-09-30 11:36:12 +0700

    first commit

---
+++ resources/views/fakultas/index.blade.php
@@ -0,0 +1,12 @@
+<table border="1">
+    <tr>
+        <th>Kode</th>
+        <th>Nama Fakultas</th>
+    </tr>
+    @foreach($fakultasData as $fakultas)
+    <tr>
+        <td>{{ $fakultas['kode'] }}</td>
+        <td>{{ $fakultas['nama_fakultas'] }}</td>
+    </tr>
+    @endforeach
+</table>
